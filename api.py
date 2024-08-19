c44d0b27e66f967ad9e8054758451f4dbe53dc43
JacobRyan102397 <rabangjacobryan23@gmail.com>
2024-08-19 19:28:33 +0800

    get data from sql

--- api.py
+++ api.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for,Response,make_response,jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'cselec'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+@app.route('/mock_data', methods={"GET"})
+def index():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM mock_data")
+    mock_data = cur.fetchall()
+    cur.close()
+
+    return make_response(jsonify(mock_data), 30)
+
+# @app.route('/')
+# def hello():
+#     return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(debug=True)
