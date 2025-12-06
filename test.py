dfe6e58be66de3b24ba33ea42b884d75de704362
JacobRyan102397 <rabangjacobryan23@gmail.com>
2025-12-06 20:55:34 +0800

    unit test test
    test lang

--- test.py
+++ test.py
@@ -19,25 +19,25 @@ class TestApp(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue("tabudo" in response.data.decode())
 
-    # def test_add_form(self):
-    #     response = self.app.get("/add_form")
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(response.data.decode(), "add.html")
+    def test_add_form(self):
+        response = self.app.get("/add_form")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), "add.html")
     
-    # def test_add(self):
-    #     response = self.app.get("/add")
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(response.data.decode(), "index.html")
+    def test_add(self):
+        response = self.app.get("/add")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), "index.html")
 
-    # def test_edit(self):
-    #     response = self.app.get("/edit/<int:id")
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(response.data.decode(), "index.html")
+    def test_edit(self):
+        response = self.app.get("/edit/<int:id")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), "index.html")
 
-    # def test_delete(self):
-    #     response = self.app.get("/delete/<int:id>")
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(response.data.decode(), "index.html")
+    def test_delete(self):
+        response = self.app.get("/delete/<int:id>")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), "index.html")
 
     if __name__ == '__main__':
         unittest.main()
